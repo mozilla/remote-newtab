c1f158e36c01aa37282e2816d63f633e430615ae
Olivier Yiptong <olivier@olivieryiptong.com>
2015-08-14 10:37:13 -0400

    use nightly and chai-as-promised in karma

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai'],
+    frameworks: ['mocha', 'chai-as-promised', 'chai'],
 
 
     // list of files / patterns to load in the browser
@@ -59,7 +59,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['FirefoxNightly'],
 
 
     // Continuous Integration mode

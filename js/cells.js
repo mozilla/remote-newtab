311eeabba8a56cf67ede5fff931345bebd3a5210
Marcos Caceres <marcos@marcosc.com>
2015-08-17 19:41:06 -0400

    Linted js/cells.js

--- js/cells.js
+++ js/cells.js
@@ -1,110 +1,130 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*globals gNewTab, gDrag, gDrop*/
 
-/**
- * This class manages a cell's DOM node (not the actually cell content, a site).
- * It's mostly read-only, i.e. all manipulation of both position and content
- * aren't handled here.
- */
-function Cell(aGrid, aNode) {
-  this._grid = aGrid;
-  this._node = aNode;
-  this._node._newtabCell = this;
-
-  // Register drag-and-drop event handlers.
-  ["dragenter", "dragover", "dragexit", "drop"].forEach(function (aType) {
-    this._node.addEventListener(aType, this, false);
-  }, this);
-}
-
-Cell.prototype = {
+"use strict";
+(function(exports) {
   /**
-   * The grid.
+   * This class manages a cell's DOM node (not the actually cell content, a site).
+   * It's mostly read-only, i.e. all manipulation of both position and content
+   * aren't handled here.
    */
-  _grid: null,
+  function Cell(aGrid, aNode) {
+    this._grid = aGrid;
+    this._node = aNode;
+    this._node._newtabCell = this;
 
-  /**
-   * The cell's DOM node.
-   */
-  get node() { return this._node; },
+    // Register drag-and-drop event handlers.
+    ["dragenter", "dragover", "dragexit", "drop"].forEach(function(aType) {
+      this._node.addEventListener(aType, this, false);
+    }, this);
+  }
 
-  /**
-   * The cell's offset in the grid.
-   */
-  get index() {
-    let index = this._grid.cells.indexOf(this);
+  Cell.prototype = {
+    /**
+     * The grid.
+     */
+    _grid: null,
 
-    // Cache this value, overwrite the getter.
-    Object.defineProperty(this, "index", {value: index, enumerable: true});
+    /**
+     * The cell's DOM node.
+     */
+    get node() {
+      return this._node;
+    },
 
-    return index;
-  },
+    /**
+     * The cell's offset in the grid.
+     */
+    get index() {
+      let index = this._grid.cells.indexOf(this);
 
-  /**
-   * The previous cell in the grid.
-   */
-  get previousSibling() {
-    let prev = this.node.previousElementSibling;
-    prev = prev && prev._newtabCell;
+      // Cache this value, overwrite the getter.
+      Object.defineProperty(this, "index", {
+        value: index,
+        enumerable: true
+      });
 
-    // Cache this value, overwrite the getter.
-    Object.defineProperty(this, "previousSibling", {value: prev, enumerable: true});
+      return index;
+    },
 
-    return prev;
-  },
+    /**
+     * The previous cell in the grid.
+     */
+    get previousSibling() {
+      let prev = this.node.previousElementSibling;
+      prev = prev && prev._newtabCell;
 
-  /**
-   * The next cell in the grid.
-   */
-  get nextSibling() {
-    let next = this.node.nextElementSibling;
-    next = next && next._newtabCell;
+      // Cache this value, overwrite the getter.
+      Object.defineProperty(this, "previousSibling", {
+        value: prev,
+        enumerable: true
+      });
 
-    // Cache this value, overwrite the getter.
-    Object.defineProperty(this, "nextSibling", {value: next, enumerable: true});
+      return prev;
+    },
 
-    return next;
-  },
+    /**
+     * The next cell in the grid.
+     */
+    get nextSibling() {
+      let next = this.node.nextElementSibling;
+      next = next && next._newtabCell;
 
-  /**
-   * The site contained in the cell, if any.
-   */
-  get site() {
-    let firstChild = this.node.firstElementChild;
-    return firstChild && firstChild._newtabSite;
-  },
+      // Cache this value, overwrite the getter.
+      Object.defineProperty(this, "nextSibling", {
+        value: next,
+        enumerable: true
+      });
 
-  /**
-   * Checks whether the cell contains a pinned site.
-   * @return Whether the cell contains a pinned site.
-   */
-  containsPinnedSite: function Cell_containsPinnedSite() {
-    let site = this.site;
-    return site && site.isPinned();
-  },
+      return next;
+    },
 
-  /**
-   * Checks whether the cell contains a site (is empty).
-   * @return Whether the cell is empty.
-   */
-  isEmpty: function Cell_isEmpty() {
-    return !this.site;
-  },
+    /**
+     * The site contained in the cell, if any.
+     */
+    get site() {
+      let firstChild = this.node.firstElementChild;
+      return firstChild && firstChild._newtabSite;
+    },
 
-  /**
-   * Handles all cell events.
-   */
-  handleEvent: function Cell_handleEvent(aEvent) {
-    // We're not responding to external drag/drop events
-    // when our parent window is in private browsing mode.
-    if (gNewTab.privateBrowsingMode && !gDrag.draggedSite)
-      return;
+    /**
+     * Checks whether the cell contains a pinned site.
+     *
+     * @return {Site} Whether the cell contains a pinned site.
+     */
+    containsPinnedSite() {
+      let site = this.site;
+      return site && site.isPinned();
+    },
 
-    if (aEvent.type != "dragexit" && !gDrag.isValid(aEvent))
-      return;
+    /**
+     * Checks whether the cell contains a site (is empty).
+     *
+     * @return {Boolean} Whether the cell is empty.
+     */
+    isEmpty() {
+      return !this.site;
+    },
 
-    switch (aEvent.type) {
+    /**
+     * Handles all cell events.
+     *
+     * @param {Event} aEvent The event to handle
+     */
+    handleEvent(aEvent) {
+      // We're not responding to external drag/drop events
+      // when our parent window is in private browsing mode.
+      if (gNewTab.privateBrowsingMode && !gDrag.draggedSite) {
+        return;
+      }
+
+      if (aEvent.type !== "dragexit" && !gDrag.isValid(aEvent)) {
+        return;
+      }
+
+      switch (aEvent.type) {
       case "dragenter":
         aEvent.preventDefault();
         gDrop.enter(this, aEvent);
@@ -119,6 +139,8 @@ Cell.prototype = {
         aEvent.preventDefault();
         gDrop.drop(this, aEvent);
         break;
+      }
     }
-  }
-};
+  };
+  exports.Cell = Cell;
+}(window));

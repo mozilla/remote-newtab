7e2cc7df4fc07e5ad752a1b3e2d1abe445739859
Marcos Caceres <marcos@marcosc.com>
2015-08-19 14:35:52 -0400

    Re-linted js/newTab.js (forgot jshint first round)

--- js/newTab.js
+++ js/newTab.js
@@ -1,32 +1,27 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*globals gGrid, gPage, gUndoDialog, gCustomize, gStrings*/
 
 "use strict";
 
-const HTML_NAMESPACE = "http://www.w3.org/1999/xhtml";
+(function(exports) {
+  const gNewTab = {
+    listeners: {},
 
-const TILES_EXPLAIN_LINK =
-  "https://support.mozilla.org/kb/how-do-tiles-work-firefox";
+    init() {
+      // Add a listener for messages sent from the browser.
+      // The listener calls our associated callback functions.
+      window.addEventListener("message", message => {
+        for (let callback of this.listeners[message.data.name]) {
+          callback(message.data.data);
+        }
+      }, false);
+    },
 
-const TILES_INTRO_LINK = "https://www.mozilla.org/firefox/tiles/";
-
-const gNewTab = {
-  listeners: {},
-
-  init() {
-    // Add a listener for messages sent from the browser.
-    // The listener calls our associated callback functions.
-    window.addEventListener("message", message => {
-      for (let callback of this.listeners[message.data.name]) {
-        callback(message.data.data);
-      }
-    }, false);
-  },
-
-  // NOTE: @emtwo Get rid of private calls to gPage members!!
-  observe(topic, data) {
-    switch(topic) {
+    // NOTE: @emtwo Get rid of private calls to gPage members!!
+    observe(topic, data) {
+      switch (topic) {
       case "page-thumbnail:create":
         if (!gGrid.ready) {
           return;
@@ -56,79 +51,85 @@ const gNewTab = {
       case "browser.newtabpage.columns":
         this.columns = data;
         break;
-    }
-
-    if ("browser.newtabpage.enabled" === topic || "browser.newtabpage.enhanced" === topic) {
-      gCustomize.updateSelected();
-    }
-  },
-
-  setState(message) {
-    this.privateBrowsingMode = message.privateBrowsingMode;
-    this.rows = message.rows;
-    this.columns = message.columns;
-    this.introShown = message.introShown;
-    this.observe("browser.newtabpage.enabled", message.enabled);
-    this.observe("browser.newtabpage.enhanced", message.enhanced);
-    gPage.init();
-  },
-
-  newTabString(name, args) {
-    let stringName = "newtab." + name;
-    if (!args) {
-      return gStrings[stringName];
-    }
-    return this._formatStringFromName(gStrings[stringName], args, args.length);
-  },
-
-  _formatStringFromName(str, substrArr) {
-    let regExp = /%[0-9]\$S/g;
-    let matches;
-    while (matches = regExp.exec(str)) {
-      let match = matches[0];
-      let index = match.charAt(1); // Get the digit in the regExp.
-      str = str.replace(match, substrArr[index - 1]);
-    }
-    return str;
-  },
-
-  stringifySites() {
-    let stringifiedSites = [];
-    for (let site of gGrid.sites) {
-      stringifiedSites.push(site ? JSON.stringify(site._link) : site);
-    }
-    return stringifiedSites;
-  },
-
-  sendToBrowser(type, data) {
-    let event = new CustomEvent("NewTabCommand", {
-      detail: {
-        command: type,
-        data: data
       }
+      let isEnhanced =  "browser.newtabpage.enhanced" === topic;
+      let isEnabled = "browser.newtabpage.enabled" === topic;
+      if (isEnabled || isEnhanced) {
+        gCustomize.updateSelected();
+      }
+    },
+
+    setState(message) {
+      this.privateBrowsingMode = message.privateBrowsingMode;
+      this.rows = message.rows;
+      this.columns = message.columns;
+      this.introShown = message.introShown;
+      this.observe("browser.newtabpage.enabled", message.enabled);
+      this.observe("browser.newtabpage.enhanced", message.enhanced);
+      gPage.init();
+    },
+
+    newTabString(name, args) {
+      let stringName = "newtab." + name;
+      if (!args) {
+        return gStrings[stringName];
+      }
+      let len = args.length;
+      return this._formatStringFromName(gStrings[stringName], args, len);
+    },
+
+    _formatStringFromName(str, substrArr) {
+      let regExp = /%[0-9]\$S/g;
+      let matches;
+      while (matches = regExp.exec(str)) { // jshint ignore:line
+        let match = matches[0];
+        let index = match.charAt(1); // Get the digit in the regExp.
+        str = str.replace(match, substrArr[index - 1]);
+      }
+      return str;
+    },
+
+    stringifySites() {
+      let stringifiedSites = [];
+      for (let site of gGrid.sites) {
+        stringifiedSites.push(site ? JSON.stringify(site._link) : site);
+      }
+      return stringifiedSites;
+    },
+
+    sendToBrowser(type, data) {
+      let event = new CustomEvent("NewTabCommand", {
+        detail: {
+          command: type,
+          data: data
+        }
+      });
+      try {
+        document.dispatchEvent(event);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+
+    registerListener(type, callback) {
+      if (!this.listeners[type]) {
+        this.listeners[type] = [];
+      }
+      this.listeners[type].push(callback);
+      this.sendToBrowser("NewTab:Register", {
+        type
+      });
+    }
+  };
+
+  // Document is loaded. Initialize the New Tab Page.
+  gNewTab.init();
+  document.addEventListener("NewTabCommandReady", () => {
+    gNewTab.registerListener("NewTab:Observe", message => {
+      gNewTab.observe(message.topic, message.data);
     });
-    try {
-      document.dispatchEvent(event);
-    } catch (e) {
-      console.log(e);
-    }
-  },
-
-  registerListener(type, callback) {
-    if (!this.listeners[type]) {
-      this.listeners[type] = [];
-    }
-    this.listeners[type].push(callback);
-    this.sendToBrowser("NewTab:Register", {type});
-  }
-};
-
-// Document is loaded. Initialize the New Tab Page.
-gNewTab.init();
-document.addEventListener("NewTabCommandReady", () => {
-  gNewTab.registerListener("NewTab:Observe", message => {
-    gNewTab.observe(message.topic, message.data);
+    gNewTab.registerListener("NewTab:State", gNewTab.setState.bind(gNewTab));
+    gNewTab.sendToBrowser("NewTab:GetInitialState");
   });
-  gNewTab.registerListener("NewTab:State", gNewTab.setState.bind(gNewTab));
-  gNewTab.sendToBrowser("NewTab:GetInitialState");
-});
+  exports.gNewTab = gNewTab;
+}(window));

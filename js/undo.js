36da3d801497dd2d37bfc35880c511dd380c0179
Marcos Caceres <marcos@marcosc.com>
2015-08-19 13:44:12 -0400

    Linted js/undo.js

--- js/undo.js
+++ js/undo.js
@@ -1,76 +1,85 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*globals gNewTab, gUpdater */
 
-/**
- * Dialog allowing to undo the removal of single site or to completely restore
- * the grid's original state.
- */
-let gUndoDialog = {
+"use strict";
+(function(exports) {
   /**
-   * The undo dialog's timeout in miliseconds.
+   * Dialog allowing to undo the removal of single site or to completely restore
+   * the grid's original state.
    */
-  HIDE_TIMEOUT_MS: 15000,
+  const gUndoDialog = {
+    /**
+     * The undo dialog's timeout in miliseconds.
+     */
+    HIDE_TIMEOUT_MS: 15000,
 
-  /**
-   * Contains undo information.
-   */
-  _undoData: null,
+    /**
+     * Contains undo information.
+     */
+    _undoData: null,
 
-  /**
-   * Initializes the undo dialog.
-   */
-  init: function UndoDialog_init() {
-    this._undoContainer = document.getElementById("newtab-undo-container");
-    this._undoContainer.addEventListener("click", this, false);
-    this._undoButton = document.getElementById("newtab-undo-button");
-    this._undoCloseButton = document.getElementById("newtab-undo-close-button");
-    this._undoRestoreButton = document.getElementById("newtab-undo-restore-button");
-    gNewTab.registerListener("NewTab:Restore", this._restore.bind(this));
-  },
+    /**
+     * Initializes the undo dialog.
+     */
+    init() {
+      // Alias QuerySelector to reduce line length
+      let qs = document.querySelector.bind(document);
+      this._undoContainer = qs("#newtab-undo-container");
+      this._undoContainer.addEventListener("click", this, false);
+      this._undoButton = qs("#newtab-undo-button");
+      this._undoCloseButton = qs("#newtab-undo-close-button");
+      this._undoRestoreButton = qs("#newtab-undo-restore-button");
+      gNewTab.registerListener("NewTab:Restore", this._restore.bind(this));
+    },
 
-  /**
-   * Shows the undo dialog.
-   * @param aSite The site that just got removed.
-   */
-  show: function UndoDialog_show(aSite) {
-    if (this._undoData)
+    /**
+     * Shows the undo dialog.
+     *
+     * @param {Site} aSite The site that just got removed.
+     */
+    show(aSite) {
+      if (this._undoData) {
+        clearTimeout(this._undoData.timeout);
+      }
+
+      this._undoData = {
+        index: aSite.cell.index,
+        wasPinned: aSite.isPinned(),
+        blockedLink: aSite.link,
+        timeout: setTimeout(this.hide.bind(this), this.HIDE_TIMEOUT_MS)
+      };
+
+      this._undoContainer.removeAttribute("undo-disabled");
+      this._undoButton.removeAttribute("tabindex");
+      this._undoCloseButton.removeAttribute("tabindex");
+      this._undoRestoreButton.removeAttribute("tabindex");
+    },
+
+    /**
+     * Hides the undo dialog.
+     */
+    hide() {
+      if (!this._undoData) {
+        return;
+      }
+
       clearTimeout(this._undoData.timeout);
+      this._undoData = null;
+      this._undoContainer.setAttribute("undo-disabled", "true");
+      this._undoButton.setAttribute("tabindex", "-1");
+      this._undoCloseButton.setAttribute("tabindex", "-1");
+      this._undoRestoreButton.setAttribute("tabindex", "-1");
+    },
 
-    this._undoData = {
-      index: aSite.cell.index,
-      wasPinned: aSite.isPinned(),
-      blockedLink: aSite.link,
-      timeout: setTimeout(this.hide.bind(this), this.HIDE_TIMEOUT_MS)
-    };
-
-    this._undoContainer.removeAttribute("undo-disabled");
-    this._undoButton.removeAttribute("tabindex");
-    this._undoCloseButton.removeAttribute("tabindex");
-    this._undoRestoreButton.removeAttribute("tabindex");
-  },
-
-  /**
-   * Hides the undo dialog.
-   */
-  hide: function UndoDialog_hide() {
-    if (!this._undoData)
-      return;
-
-    clearTimeout(this._undoData.timeout);
-    this._undoData = null;
-    this._undoContainer.setAttribute("undo-disabled", "true");
-    this._undoButton.setAttribute("tabindex", "-1");
-    this._undoCloseButton.setAttribute("tabindex", "-1");
-    this._undoRestoreButton.setAttribute("tabindex", "-1");
-  },
-
-  /**
-   * The undo dialog event handler.
-   * @param aEvent The event to handle.
-   */
-  handleEvent: function UndoDialog_handleEvent(aEvent) {
-    switch (aEvent.target.id) {
+    /**
+     * The undo dialog event handler.
+     *
+     * @param {Event} aEvent The event to handle.
+     */
+    handleEvent(aEvent) {
+      switch (aEvent.target.id) {
       case "newtab-undo-button":
         this._undo();
         break;
@@ -80,35 +89,44 @@ let gUndoDialog = {
       case "newtab-undo-close-button":
         this.hide();
         break;
-    }
-  },
+      }
+    },
 
-  /**
-   * Undo the last blocked site.
-   */
-  _undo: function UndoDialog_undo() {
-    if (!this._undoData)
-      return;
+    /**
+     * Undo the last blocked site.
+     */
+    _undo() {
+      if (!this._undoData) {
+        return;
+      }
 
-    let {index, wasPinned, blockedLink} = this._undoData;
-    gNewTab.sendToBrowser("NewTab:UnblockLink", {link: blockedLink, wasPinned, index});
-    this._restore();
-  },
+      let {
+        index, wasPinned, blockedLink
+      } = this._undoData;
+      gNewTab.sendToBrowser("NewTab:UnblockLink", {
+        link: blockedLink,
+        wasPinned,
+        index
+      });
+      this._restore();
+    },
 
-  /**
-   * Undo all blocked sites.
-   */
-  _undoAll: function UndoDialog_undoAll() {
-    gNewTab.sendToBrowser("NewTab:UndoAll");
-  },
+    /**
+     * Undo all blocked sites.
+     */
+    _undoAll() {
+      gNewTab.sendToBrowser("NewTab:UndoAll");
+    },
 
-  /**
-   * Restore the state of the page and hide the undo dialog.
-   */
-  _restore: function UndoDialog_restore() {
-    gUpdater.sendUpdate();
-    this.hide();
-  },
-};
+    /**
+     * Restore the state of the page and hide the undo dialog.
+     */
+    _restore() {
+      gUpdater.sendUpdate();
+      this.hide();
+    },
+  };
 
-gUndoDialog.init();
+  gUndoDialog.init();
+  exports.gUndoDialog = gUndoDialog;
+}(window));

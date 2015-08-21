ba3ba0c10e388b177c5f3acb9a78e90c3591b6eb
Marina Samuel <msamuel@mozilla.com>
2015-08-21 16:55:42 -0400

    Update lint comments.

--- js/page.js
+++ js/page.js
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-/*global gNewTab, gGrid, gIntro, gDrag, gCustomize, gUpdater, gDropTargetShim */
+/*global gNewTab, gGrid, gIntro, gDrag, gCustomize, gUndoDialog, gUpdater, gDropTargetShim */
 
 "use strict";
 (function(exports) {

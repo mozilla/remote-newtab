074ff920994dfad03d52e830f3acd79191f549a5
Ursula Sarracini <ursulasarracini@gmail.com>
2015-10-06 18:33:57 -0400

    fix search controller init

--- js/search.js
+++ js/search.js
@@ -5,12 +5,15 @@
 "use strict";
 (function(exports) {
   let gSearch = {
+
+    _contentSearchController: null,
+
     init() {
-      document.getElementById("newtab-search-submit")
-        .addEventListener("click", e => this._contentSearchController.search(e));
       let textbox = document.getElementById("newtab-search-text");
       this._contentSearchController =
         new ContentSearchUIController(textbox, textbox.parentNode, "newtab", "newtab");
+      document.getElementById("newtab-search-submit")
+        .addEventListener("click", e => this._contentSearchController.search(e));
     },
   };
   exports.gSearch = gSearch;

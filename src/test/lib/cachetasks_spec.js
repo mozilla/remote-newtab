46704f0efb62f8371ea40c0bdbafc51c09a34524
Marcos Caceres <marcos@marcosc.com>
2015-10-31 22:22:40 +1100

    Added tests for isStale, etc. to cachetasks_spec.js

--- src/test/lib/cachetasks_spec.js
+++ src/test/lib/cachetasks_spec.js
@@ -1,118 +1,104 @@
 /* jshint esnext: true, expr: true */
-/* globals expect, fetch, async, it, CacheTasks, caches, Request, Response*/
+/* globals expect, async, it, CacheTasks, caches, Request, Response*/
 "use strict";
 const CACHENAME = "test";
-describe("CacheTasks", function() {
-  // Helper functions
 
-  describe("deleteAllCaches() method", () => {
-    it("should delete all caches.", async(function*() {
+function testType(type) {
+  return (method) => {
+    return method(type);
+  };
+}
+var tUndefined = testType(undefined);
+var tString = testType("string");
+var tNumber = testType(123);
+var tArray = testType([]);
+var tNull = testType(null);
+var tFakeObj = testType({
+  headers: new Map()
+});
+var tRequest = testType(new Request(""));
+
+describe("CacheTasks", function() {
+  describe("deleteCaches() method", () => {
+    it("should delete all caches when no argument is given.", async(function*() {
       var cacheNames = [CACHENAME, "foo", "bar", "baz", "bing"];
       for (var name of cacheNames) {
         yield caches.open(name);
       }
-      var result = yield CacheTasks.deleteAllCaches();
-      var checkNameValue = function([name, value]) {
-        return cacheNames.includes(name) && value === true;
-      };
-      expect(Array.from(result.entries()).every(checkNameValue)).to.be.true;
+      var result = yield CacheTasks.deleteCaches();
+      var checkNameValue = async(function*([name, value]) {
+        var hasCache = yield caches.has(name);
+        return cacheNames.includes(name) && value === true && hasCache === false;
+      });
+      for (var entry of result.entries()) {
+        expect(yield checkNameValue(entry)).to.be.true;
+      }
+      var keys = yield caches.keys();
+      expect(keys.length).to.equal(0);
+    }));
+    it("should delete the given caches.", async(function*() {
+      var cachesToKeep = ["keep1", "keep2"];
+      var cachesToDelete = ["delete1", "delete2"];
+      for (var name of cachesToKeep.concat(cachesToDelete)) {
+        yield caches.open(name);
+      }
+      var result = yield CacheTasks.deleteCaches(cachesToDelete);
+      var keys = yield caches.keys();
+      expect(keys).to.eql(cachesToKeep);
+      result = yield CacheTasks.deleteCaches(cachesToKeep);
+      keys = yield caches.keys();
+      expect(keys.length).to.equal(0);
     }));
   });
 
-  describe("populateCache() method", () => {
-    it("should allow populating the cache", async(function*() {
-      var result = yield CacheTasks.populateCache(["js/lib/async.js"], CACHENAME);
+  describe("addAll() method", () => {
+    it("should allow populating the cache.", async(function*() {
+      var result = yield CacheTasks.addAll(["js/lib/async.js"], CACHENAME);
       expect(result).to.be.true;
     }));
   });
 
-  describe("respondFromCache() method", () => {
+  describe("match() method", () => {
     it("should respond when given a Request object.", async(function*() {
       var request = new Request("js/lib/async.js");
-      var result = yield CacheTasks.respondFromCache(request, CACHENAME);
-      expect(result.url === request.url).to.be.true;
+      var result = yield CacheTasks.match(request, CACHENAME);
+      expect(result.url).to.equal(request.url);
     }));
     it("should respond when given a string.", async(function*() {
       var request = "js/lib/async.js";
-      var result = yield CacheTasks.respondFromCache(request, CACHENAME);
-      expect(result.url === new URL(request, window.location).href).to.be.true;
+      var result = yield CacheTasks.match(request, CACHENAME);
+      var expectedURL = new URL(request, window.location).href;
+      expect(result.url).to.equal(expectedURL);
     }));
     it("should respond when given a URL.", async(function*() {
       var request = new URL("js/lib/async.js", window.location);
-      var result = yield CacheTasks.respondFromCache(request, CACHENAME);
-      expect(result.url === request.href).to.be.true;
+      var result = yield CacheTasks.match(request, CACHENAME);
+      expect(result.url).to.equal(request.href);
     }));
-    it("should reject when asked to respond with a missing entry.", () => {
-      var promise = CacheTasks.respondFromCache("this/is/not/real", CACHENAME);
-      promise.should.be.rejectedWith(Error);
-    });
+    it("should return undefined with missing entry.", async(function*() {
+      var result = yield CacheTasks.match("no-such-entry", CACHENAME);
+      expect(result).to.equal(undefined);
+    }));
   });
 
-  describe("hasCacheEntry() method", () => {
+  describe("has() method", () => {
     it("should store entries in the cache.", async(function*() {
-      var result = yield CacheTasks.hasCacheEntry("js/lib/async.js", CACHENAME);
+      var result = yield CacheTasks.has("js/lib/async.js", CACHENAME);
       expect(result).to.be.true;
-      result = yield CacheTasks.hasCacheEntry("/not/stored", CACHENAME);
+      result = yield CacheTasks.has("/not/stored", CACHENAME);
       expect(result).to.be.false;
     }));
   });
 
-  describe("refreshCacheEntry() method", () => {
-    it("should reject if the entry is missing", () => {
-      var promise = CacheTasks.refreshCacheEntry("this/is/not/real", CACHENAME);
-      promise.should.be.rejectedWith(Error);
-    });
-    it("should return responses that are not stale without refreshing", async(function*() {
-      var thePast = new Date(Date.now() - 100000).toGMTString();
-      var request = new Request("fresh");
-      var response = new Response("fresh", {
-        headers: {
-          "Date": thePast,
-          "Test": "pass"
-        }
-      });
-      yield CacheTasks.putCacheEntry(request, response, CACHENAME);
-      var result = yield CacheTasks.refreshCacheEntry(request, CACHENAME);
-      expect(result.headers.get("Date")).to.equal(thePast);
-      expect(result.headers.get("Test")).to.equal("pass");
-    }));
-    it("should reject when refreshing an uncacheble response.", async(function*() {
-      var request = "js/lib/async.js";
-      var response = yield fetch(request);
-      var thePast = new Date(Date.now() - 10000).toGMTString();
-      response.headers.set("Expires", thePast);
-      yield CacheTasks.putCacheEntry(request, response, CACHENAME);
-      var promise = CacheTasks.refreshCacheEntry(request, CACHENAME);
-      promise.should.eventually.be.rejectedWith(Error);
-    }));
-    it("should reject when refreshing an uncacheble response even with force.", async(function*() {
-      var request = "js/lib/async.js";
-      var response = yield fetch(request);
-      var thePast = new Date(Date.now() - 10000).toGMTString();
-      response.headers.set("Expires", thePast);
-      yield CacheTasks.putCacheEntry(request, response, CACHENAME);
-      var promise = CacheTasks.refreshCacheEntry(request, CACHENAME, "force");
-      promise.should.eventually.be.rejectedWith(Error);
-    }));
-  });
-
-  describe("putCacheEntry() method", () => {
-    var request = new Request("test", {
-      headers: {
-        "test": "requestPass"
-      }
-    });
-    var response = new Response("test", {
-      headers: {
-        "test": "responsePass"
-      }
-    });
+  describe("put() method", () => {
+    var request = new Request("test");
     it("should put cachable entries in the cache.", async(function*() {
-      var result = yield CacheTasks.putCacheEntry(request, response, CACHENAME);
+      var response = new Response("test");
+      var result = yield CacheTasks.put(request, response, CACHENAME);
       expect(result).to.be.true;
     }));
     it("should now have 'test' in the cache.", async(function*() {
-      var result = yield CacheTasks.hasCacheEntry(request, CACHENAME);
+      var result = yield CacheTasks.has(request, CACHENAME);
       expect(result).to.be.true;
     }));
     it("should return false the request forbids caching", async(function*() {
@@ -121,12 +107,8 @@ describe("CacheTasks", function() {
           "Cache-Control": "no-store"
         }
       });
-      var response = new Response("test", {
-        headers: {
-          "test": "responsePass"
-        }
-      });
-      var result = yield CacheTasks.putCacheEntry(uncachableRequest, response, CACHENAME);
+      var response = new Response("test");
+      var result = yield CacheTasks.put(uncachableRequest, response, CACHENAME);
       expect(result).to.be.false;
     }));
     it("should return false the response forbids caching", async(function*() {
@@ -135,12 +117,7 @@ describe("CacheTasks", function() {
           "Cache-Control": "no-store"
         }
       });
-      var request = new Request("test", {
-        headers: {
-          "test": "requestPass"
-        }
-      });
-      var result = yield CacheTasks.putCacheEntry(request, uncachableResponse, CACHENAME);
+      var result = yield CacheTasks.put("test", uncachableResponse, CACHENAME);
       expect(result).to.be.false;
     }));
     it("should return false the response and response forbids caching", async(function*() {
@@ -154,21 +131,147 @@ describe("CacheTasks", function() {
           "Cache-Control": "no-store"
         }
       });
-      var result = yield CacheTasks.putCacheEntry(uncachableRequest, uncachableResponse, CACHENAME);
+      var result = yield CacheTasks.put(uncachableRequest, uncachableResponse, CACHENAME);
       expect(result).to.be.false;
     }));
+    it("should return true if the max-age is still valid.", async(function*() {
+      var request = new Request("test");
+      var response = new Response(".");
+      var beforeNow = new Date(Date.now() - 100000);
+      var afterNow = new Date(Date.now() + 100000);
+      var maxAge = (afterNow - beforeNow) / 1000; //to seconds
+      response.headers.set("Date", beforeNow.toGMTString());
+      response.headers.set("Cache-Control", `max-age=${maxAge}`);
+      var result = yield CacheTasks.put(request, response);
+      expect(result).to.be.true;
+    }));
+    it("should put a response that has not expired.", async(function*() {
+      var response = new Response(".");
+      var theFuture = new Date(Date.now() + 100000).toGMTString();
+      var now = new Date(Date.now()).toGMTString();
+      response.headers.set("Date", now);
+      response.headers.set("Expires", theFuture);
+      var result = yield CacheTasks.put(request, response);
+      expect(result).to.be.true;
+    }));
+    it("should put a response that has expired.", async(function*() {
+      var response = new Response(".");
+      var thePast = new Date(Date.now() - 100000).toGMTString();
+      var now = new Date(Date.now()).toGMTString();
+      response.headers.set("Date", now);
+      response.headers.set("Expires", thePast);
+      var result = yield CacheTasks.put(request, response);
+      expect(result).to.be.true;
+    }));
   });
-
-  describe("deleteCacheEntry() method", () => {
+  describe("isStale() method", () => {
+    it("should throw given invalid input", () => {
+      var f = CacheTasks.isStale;
+      expect(() => tUndefined(f)).to.throw(TypeError);
+      expect(() => tString(f)).to.throw(TypeError);
+      expect(() => tNumber(f)).to.throw(TypeError);
+      expect(() => tArray(f)).to.throw(TypeError);
+      expect(() => tNull(f)).to.throw(TypeError);
+      expect(() => tFakeObj(f)).to.throw(TypeError);
+      expect(() => tRequest(f)).to.throw(TypeError);
+    });
+    it("should return true when the Expires date is in the past.", () => {
+      var response = new Response(".");
+      var thePast = new Date(Date.now() - 10000).toGMTString();
+      response.headers.set("Expires", thePast);
+      expect(CacheTasks.isStale(response)).to.be.true;
+    });
+    it("should return false when the Expires date is in the future.", () => {
+      var response = new Response(".");
+      var theFuture = new Date(Date.now() + 100000).toGMTString();
+      response.headers.set("Expires", theFuture);
+      expect(CacheTasks.isStale(response)).to.be.false;
+    });
+    it("should return false when the Expires date is now.", () => {
+      var response = new Response(".");
+      var now = new Date(Date.now()).toGMTString();
+      response.headers.set("Expires", now);
+      expect(CacheTasks.isStale(response)).to.be.false;
+    });
+    it("should throw when the Expires date not a number.", () => {
+      var response = new Response(".");
+      var date = "invalid date!";
+      response.headers.set("Expires", date);
+      expect(
+        () => CacheTasks.isStale(response)
+      ).to.throw(TypeError);
+    });
+    it("should throw if there is no max-age header.", () => {
+      var response = new Response(".");
+      response.headers.set("Cache-Control", "not-max-age=0");
+      expect(
+        () => CacheTasks.isStale(response)
+      ).to.throw(Error);
+    });
+    it("should throw if there is no Date header.", () => {
+      var response = new Response(".");
+      response.headers.set("Cache-Control", "max-age=123");
+      expect(
+        () => CacheTasks.isStale(response)
+      ).to.throw(Error);
+    });
+    it("should throw if Date header can't be parsed.", () => {
+      var response = new Response(".");
+      response.headers.set("Cache-Control", "max-age=123");
+      response.headers.set("Date", "invalid");
+      expect(
+        () => CacheTasks.isStale(response)
+      ).to.throw(Error);
+    });
+    it("should throw if max-age directive can't be parsed.", () => {
+      var response = new Response(".");
+      var now = new Date(Date.now()).toGMTString();
+      response.headers.set("Date", now);
+      response.headers.set("Cache-Control", "max-age=invalid");
+      expect(
+        () => CacheTasks.isStale(response)
+      ).to.throw(Error);
+    });
+    it("should return true if the max-age has passed.", () => {
+      var response = new Response(".");
+      var longBeforeNow = new Date(Date.now() - 1000000);
+      var aBitBeforeNow = new Date(Date.now() - 10000);
+      var maxAge = (aBitBeforeNow - longBeforeNow) / 1000; //to seconds
+      response.headers.set("Date", longBeforeNow.toGMTString());
+      response.headers.set("Cache-Control", `max-age=${maxAge}`);
+      expect(CacheTasks.isStale(response)).to.be.true;
+    });
+    it("should return false if the max-age hasn't passed.", () => {
+      var response = new Response(".");
+      var beforeNow = new Date(Date.now() - 100000);
+      var afterNow = new Date(Date.now() + 100000);
+      var maxAge = (afterNow - beforeNow) / 1000; //to seconds
+      response.headers.set("Date", beforeNow.toGMTString());
+      response.headers.set("Cache-Control", `max-age=${maxAge}`);
+      expect(CacheTasks.isStale(response)).to.be.false;
+    });
+    it("should return false if the max-age is 'now'.", () => {
+      var response = new Response(".");
+      var now = new Date(Date.now());
+      response.headers.set("Date", now.toGMTString());
+      response.headers.set("Cache-Control", "max-age=0");
+      expect(CacheTasks.isStale(response)).to.be.false;
+    });
+    it("should return false if there is no cache information.", () => {
+      var response = new Response(".");
+      expect(CacheTasks.isStale(response)).to.be.false;
+    });
+  });
+  describe("delete() method", () => {
     it("should allow deleting cache entries", async(function*() {
       var request = new Request("deleteTest");
       var response = new Response("deleteTest");
-      yield CacheTasks.putCacheEntry(request, response, CACHENAME);
-      var hasEntry = yield CacheTasks.hasCacheEntry(request, CACHENAME);
+      yield CacheTasks.put(request, response, CACHENAME);
+      var hasEntry = yield CacheTasks.has(request, CACHENAME);
       expect(hasEntry).to.be.true;
-      var success = yield CacheTasks.deleteCacheEntry(request, CACHENAME);
+      var success = yield CacheTasks.delete(request, CACHENAME);
       expect(success).to.be.true;
-      hasEntry = yield CacheTasks.hasCacheEntry(request, CACHENAME);
+      hasEntry = yield CacheTasks.has(request, CACHENAME);
       expect(hasEntry).to.be.false;
     }));
   });

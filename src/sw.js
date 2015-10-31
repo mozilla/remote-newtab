85c1b041fe81d15547a5357c316d0c34513ac95b
Marcos Caceres <marcos@marcosc.com>
2015-10-31 22:26:01 +1100

    Rewrote how responses are selected in sw.js

--- src/sw.js
+++ src/sw.js
@@ -4,70 +4,64 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 /*jshint worker:true*/
-/*globals async, CacheTasks, self, mainSiteURLs, Response, fetch */
-
+/*globals async, CacheTasks, Response, fetch */
 "use strict";
-
 importScripts("js/lib/async.js"); // imports async()
-importScripts("js/lib/responseRequestUtils.js"); // imports RequestUtils, ResponseUtils
 importScripts("js/lib/cachetasks.js"); // imports CacheTasks
-importScripts("js/mainSiteURLs.js"); // imports mainSiteURLs
 
-const PageThumbTasks = {
+const SWTasks = {
   /**
-   * Ensure we always resolve with a Response, to avoid Content Corrupted errors
-   * being shown to the user.
-   *
-   * @param {Promise<Response>} promise The promise that should resolve to a
-   *                                    Response.
+   * Initialization tasks for service workers.
    */
-  ensureResponse: async(function*(promise) {
-    var response;
-    try {
-      response = yield promise;
-    }catch (err) {
-      console.error(err);
-      response = new Response();
-    }
-    return response;
-  }),
-  init: async(function*() {
-    yield CacheTasks.deleteAllCaches();
-    yield CacheTasks.populateCache(mainSiteURLs, "skeleton_cache");
-  }),
+  init() {
+    return async.task(function*() {
+      yield CacheTasks.deleteCaches([
+        "skeleton_cache", // The html, js, css, of site
+        "pagethumbs_cache", // User's history tiles
+        "ads_cache", // Advertisement tiles
+      ]);
+      var request = yield fetch("js/mainSiteURLs.json");
+      var mainSiteURLs = (yield request.json())
+        .map(path => `${location.origin}${path}`);
+      yield CacheTasks.addAll(mainSiteURLs, "skeleton_cache");
+    }, this);
+  },
+  /**
+   * Selects the appropriate response to return.
+   *
+   * @param {Request} request The request that the client made.
+   * @return {Promise<response>} The response to serve to the client.
+   */
+  selectResponse(request) {
+    return async.task(function*() {
+      var response;
+      var key = new URL(request.url).pathname.split("/")[1];
+      switch (key) {
+      case "pagethumbs":
+        response = yield CacheTasks.match(request, "pagethumbs_cache");
+        // TODO: Ensure page thumb.
+        break;
+      case "images":
+        response = yield CacheTasks.match(request, "ads_cache");
+        // TODO: Ensure dynamic tile
+        break;
+      default:
+        response = yield CacheTasks.match(request, "skeleton_cache");
+      }
+      if (!response && navigator.onLine) {
+        console.warn(`Going to network for ${request.url}`);
+        response = yield fetch(request);
+        yield CacheTasks.put(request, response, "skeleton_cache");
+      }
+      return response || new Response();
+    }, this);
+  },
 };
 
 self.addEventListener("install", (ev) => {
-  ev.waitUntil(PageThumbTasks.init());
+  ev.waitUntil(SWTasks.init());
 });
 
 self.addEventListener("fetch", (ev) => {
-  var key = getSwitchKeyFromURL(ev.request.url);
-  var promise;
-  switch (key) {
-  case "pagethumbs":
-    promise = CacheTasks.respondFromCache(ev.request, "thumbs_cache");
-    ev.respondWith(PageThumbTasks.ensureResponse(promise));
-    break;
-  case "images":
-    promise = CacheTasks.refreshCacheEntry(ev.request, "tiles_cache");
-    ev.respondWith(PageThumbTasks.ensureResponse(promise));
-    break;
-  case "karma":
-    ev.respondWith(fetch(ev.request));
-    break;
-  default:
-    promise = CacheTasks.refreshCacheEntry(ev.request, "skeleton_cache");
-    ev.respondWith(promise);
-  }
+  ev.respondWith(SWTasks.selectResponse(ev.request));
 });
-
-function getSwitchKeyFromURL(url) {
-  //check if we are running in Karma
-  if (self.location.port === "9876") {
-    return "karma";
-  }
-  // split and return the first path segment
-  var key = new URL(url).pathname.split("/")[1];
-  return key;
-}

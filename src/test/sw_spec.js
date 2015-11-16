bbdb449307206c2f835414b2f961984947f6813f
Marcos Caceres <marcos@marcosc.com>
2015-11-16 23:58:48 +1100

    After registering, unregister, as to not screw up other tests

--- src/test/sw_spec.js
+++ src/test/sw_spec.js
@@ -4,8 +4,9 @@
 describe("Service worker registration", function() {
   it("should correctly register and activate the Service Worker.", async(function*() {
     // force reload on first load.
-    navigator.serviceWorker.register("sw.js");
+    var registration = yield navigator.serviceWorker.register("sw.js");
     var sw = (yield navigator.serviceWorker.ready).active;
     expect(sw).to.be.ok;
+    yield registration.unregister();
   }));
 });

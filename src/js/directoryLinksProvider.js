6c16ae84b1de25bb5c6ab859fef5d50872a60a77
Marina Samuel <msamuel@mozilla.com>
2015-11-18 10:41:03 -0500

    Move DirectoryLinksProvider into anonymous function.

--- src/js/directoryLinksProvider.js
+++ src/js/directoryLinksProvider.js
@@ -1,8 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
- /*globals Request, CacheTasks, async, ProviderManager, Links, fetch*/
-
+ /*jshint worker:true*/
+ /*globals CacheTasks, async, ProviderManager, Links, fetch*/
 "use strict";
 
 // The const that tells where to obtain directory links
@@ -11,120 +11,123 @@ const PREF_DIRECTORY_SOURCE = "https://tiles.services.mozilla.com/v3/links/fetch
 // The frecency of a directory link
 const DIRECTORY_FRECENCY = 1000;
 
-/**
- * Emits notifications to PlacesProvider and Links
- */
-const DirectoryLinksProvider = {
-  _globalFetch: fetch,
-
-  _observers: new Set(),
-
-  _directoryLinksRequest: new Request(PREF_DIRECTORY_SOURCE),
-
+(function(exports) {
   /**
-   * A mapping from eTLD+1 to an enhanced link objects
+   * Emits notifications to PlacesProvider and Links
    */
-  _enhancedLinks: new Map(),
+  const DirectoryLinksProvider = {
+    _globalFetch: fetch,
 
-  /**
-   * A mapping from site to a list of suggested link objects
-   */
-  _suggestedLinks: new Map(),
+    _observers: new Set(),
 
-  init: async(function*(mockFetch) {
-    var fetch = mockFetch || DirectoryLinksProvider._globalFetch;
+    _directoryLinksRequest: PREF_DIRECTORY_SOURCE,
 
-    Links.addObserver(DirectoryLinksProvider);
-    var response;
-    var directoryLinksInCache = yield CacheTasks.has(PREF_DIRECTORY_SOURCE, "directory_cache");
-    if (!directoryLinksInCache) {
-      response = yield fetch(DirectoryLinksProvider._directoryLinksRequest);
-      var result = yield CacheTasks.put(PREF_DIRECTORY_SOURCE, response, "directory_cache");
-      if (!result) {
-        console.warn("Failed to store directory links");
+    /**
+     * A mapping from eTLD+1 to an enhanced link objects
+     */
+    _enhancedLinks: new Map(),
+
+    /**
+     * A mapping from site to a list of suggested link objects
+     */
+    _suggestedLinks: new Map(),
+
+    init: async(function*(mockFetch) {
+      var fetch = mockFetch || DirectoryLinksProvider._globalFetch;
+
+      Links.addObserver(DirectoryLinksProvider);
+      var response;
+      var directoryLinksInCache = yield CacheTasks.has(PREF_DIRECTORY_SOURCE, "directory_cache");
+      if (!directoryLinksInCache) {
+        response = yield fetch(DirectoryLinksProvider._directoryLinksRequest);
+        var result = yield CacheTasks.put(PREF_DIRECTORY_SOURCE, response, "directory_cache");
+        if (!result) {
+          console.warn("Failed to store directory links");
+        }
       }
-    }
-    response = yield CacheTasks.match(DirectoryLinksProvider._directoryLinksRequest, "directory_cache");
-    var text = yield response.text();
-    DirectoryLinksProvider._links = JSON.parse(text);
-  }),
+      response = yield CacheTasks.match(DirectoryLinksProvider._directoryLinksRequest, "directory_cache");
+      var text = yield response.text();
+      DirectoryLinksProvider._links = JSON.parse(text);
+    }),
 
-  /**
-   * Get the enhanced link object for a link (whether history or directory)
-   */
-  getEnhancedLink(link) {
-    // Use the provided link if it's already enhanced
-    return link.enhancedImageURI && link ? link :
-           this._enhancedLinks.get(ProviderManager.extractSite(link.url));
-  },
+    /**
+     * Get the enhanced link object for a link (whether history or directory)
+     */
+    getEnhancedLink(link) {
+      // Use the provided link if it's already enhanced
+      return link.enhancedImageURI && link ? link :
+             this._enhancedLinks.get(ProviderManager.extractSite(link.url));
+    },
 
-  _cacheSuggestedLinks(link) {
-    // Don't cache links that don't have the expected 'frecent_sites'
-    //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-    if (!link.frecent_sites) {
-      return;
-    }
-
-    for (var suggestedSite of link.frecent_sites) {
-      var suggestedMap = this._suggestedLinks.get(suggestedSite) || new Map();
-      suggestedMap.set(link.url, link);
-      this._suggestedLinks.set(suggestedSite, suggestedMap);
-    }
-    //jscs:enable requireCamelCaseOrUpperCaseIdentifiers
-  },
-
-  _escapeChars(text) {
-    let charMap = {
-      "&": "&amp;",
-      "<": "&lt;",
-      ">": "&gt;",
-      "\"": "&quot;",
-      "'": "&#039;"
-    };
-
-    return text.replace(/[&<>"']/g, (character) => charMap[character]);
-  },
-
-  /**
-   * Gets the current set of directory links.
-   */
-  getLinks: async(function*() {
-    var rawLinks = DirectoryLinksProvider._links;
-
-    // Reset the cache of suggested tiles and enhanced images for this new set of links
-    DirectoryLinksProvider._enhancedLinks.clear();
-    DirectoryLinksProvider._suggestedLinks.clear();
-
-    rawLinks.suggested.forEach((link, position) => {
-      // Suggested sites must have an adgroup name.
+    _cacheSuggestedLinks(link) {
+      // Don't cache links that don't have the expected 'frecent_sites'
       //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-      if (!link.adgroup_name) {
+      if (!link.frecent_sites) {
         return;
       }
 
-      link.explanation = DirectoryLinksProvider._escapeChars(link.explanation);
-      link.targetedName = DirectoryLinksProvider._escapeChars(link.adgroup_name);
-      link.lastVisitDate = rawLinks.suggested.length - position;
+      for (var suggestedSite of link.frecent_sites) {
+        var suggestedMap = this._suggestedLinks.get(suggestedSite) || new Map();
+        suggestedMap.set(link.url, link);
+        this._suggestedLinks.set(suggestedSite, suggestedMap);
+      }
       //jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+    },
 
-      // We cache suggested tiles here but do not push any of them in the links list yet.
-      // The decision for which suggested tile to include will be made separately.
-      DirectoryLinksProvider._cacheSuggestedLinks(link);
-    });
+    _escapeChars(text) {
+      let charMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        "\"": "&quot;",
+        "'": "&#039;"
+      };
 
-    var links = rawLinks.directory.map((link, position) => {
-      link.lastVisitDate = rawLinks.length - position;
-      link.frecency = DIRECTORY_FRECENCY;
-      return link;
-    });
+      return text.replace(/[&<>"']/g, (character) => charMap[character]);
+    },
 
-    // Allow for one link suggestion on top of the default directory links
-    DirectoryLinksProvider.maxNumLinks = links.length + 1;
+    /**
+     * Gets the current set of directory links.
+     */
+    getLinks: async(function*() {
+      var rawLinks = DirectoryLinksProvider._links;
 
-    return links;
-  }),
+      // Reset the cache of suggested tiles and enhanced images for this new set of links
+      DirectoryLinksProvider._enhancedLinks.clear();
+      DirectoryLinksProvider._suggestedLinks.clear();
 
-  addObserver(aObserver) {
-    this._observers.add(aObserver);
-  },
-};
+      rawLinks.suggested.forEach((link, position) => {
+        // Suggested sites must have an adgroup name.
+        //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+        if (!link.adgroup_name) {
+          return;
+        }
+
+        link.explanation = DirectoryLinksProvider._escapeChars(link.explanation);
+        link.targetedName = DirectoryLinksProvider._escapeChars(link.adgroup_name);
+        link.lastVisitDate = rawLinks.suggested.length - position;
+        //jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+
+        // We cache suggested tiles here but do not push any of them in the links list yet.
+        // The decision for which suggested tile to include will be made separately.
+        DirectoryLinksProvider._cacheSuggestedLinks(link);
+      });
+
+      var links = rawLinks.directory.map((link, position) => {
+        link.lastVisitDate = rawLinks.length - position;
+        link.frecency = DIRECTORY_FRECENCY;
+        return link;
+      });
+
+      // Allow for one link suggestion on top of the default directory links
+      DirectoryLinksProvider.maxNumLinks = links.length + 1;
+
+      return links;
+    }),
+
+    addObserver(aObserver) {
+      this._observers.add(aObserver);
+    },
+  };
+  exports.DirectoryLinksProvider = DirectoryLinksProvider;
+}(self));

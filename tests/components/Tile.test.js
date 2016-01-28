5e6cc93dc691848b586f05771be5042560420420
Marina Samuel <msamuel@mozilla.com>
2016-01-28 13:47:24 -0500

    Fix Tile test.

--- tests/components/Tile.test.js
+++ tests/components/Tile.test.js
@@ -1,12 +1,20 @@
 const assert = require("chai").assert;
 const Tile = require("components/Tile/Tile");
 const React = require("react");
+const {combineReducers} = require("redux");
 const ReactDOM = require("react-dom");
 const {shouldConsoleError} = require('tests/test-utils');
+const finalCreateStore = require("lib/finalCreateStore");
+
+const reducers = require("reducers/index");
+const reducer = combineReducers(reducers);
+const store = finalCreateStore(reducer);
+
 const fakeProps = {
   title: "My tile",
   imageURI: "https://foo.com/foo.jpg",
-  url: "https://foo.com"
+  url: "https://foo.com",
+  store
 };
 
 describe("Tile", () => {
